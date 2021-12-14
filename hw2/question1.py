ba4f9d292fa39dd6c2208a28270d7e5ceb405c39
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2021-12-14 10:35:59 +0300

    hw2 initialized, gpg test

---
+++ hw2/question1.py
@@ -0,0 +1,9 @@
+# Ozan Yetkin | 1908227
+# Write a recursive function that generates a list of all anagrams of a given string.
+
+def anagrams_of(s):
+	# You implement this part
+    pass
+
+# Test should return the list of all anagrams
+print(anagrams_of("pots"))
