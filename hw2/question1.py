71988f3b8cb8c1ebe4ac11a1e15559ca5918b933
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2021-12-14 12:25:57 +0300

    recursion initialized for hw2 q1

--- hw2/question1.py
+++ hw2/question1.py
@@ -2,8 +2,11 @@
 # Write a recursive function that generates a list of all anagrams of a given string.
 
 def anagrams_of(s):
-	# You implement this part
-    pass
+	# Define the base case which is basically an empty string
+    if len(s) == 1:
+        return [s]
+    else:
+        return anagrams_of(s[1:]) + anagrams_of(s[0])
 
 # Test should return the list of all anagrams
 print(anagrams_of("pots"))
