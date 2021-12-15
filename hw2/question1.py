3a7c340f0665ad89a564459ea92c42289240b605
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2021-12-15 11:50:06 +0300

    hw2 q1 finished

--- hw2/question1.py
+++ hw2/question1.py
@@ -3,13 +3,15 @@
 
 def anagrams_of(s):
 	# Define the base case which is basically an empty string
-    if len(s) == 0:
-        return []
+    if len(s) == 1:
+        return [s]
     else:
-        anagrams = anagrams_of(s[:-1])
-        for i in range(len(s)):
-            anagrams.append(s[i:] + s[i])
+        anagrams = []
+        for anagram in anagrams_of(s[:-1]):
+            for i in range(len(s)):
+                print(s[i])
+                anagrams.append(anagram[i:] + s[-1] + anagram[:i])
         return anagrams
 
 # Test should return the list of all anagrams
-print(anagrams_of("po"))
+print(anagrams_of("pots"))
