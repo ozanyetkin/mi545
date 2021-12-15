084aa8963d1cfcb7cd7b6a0d70ef55724ff30370
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2021-12-15 11:59:59 +0300

    hw2 q1 finished

--- hw2/question1.py
+++ hw2/question1.py
@@ -2,15 +2,20 @@
 # Write a recursive function that generates a list of all anagrams of a given string.
 
 def anagrams_of(s):
-	# Define the base case which is basically an empty string
-    if len(s) == 1:
+	# Define the base case which is basically a string with a single character
+    if len(s) <= 1:
+        # Return the list containing the string itself if it is the base case
         return [s]
     else:
+        # If it is not the base case, initialize an empty list that would contain anagrams
         anagrams = []
+        # Iterate over the already found anagrams recursively by calling the function itself by trimming the last character
         for anagram in anagrams_of(s[:-1]):
+            # Iterate again with the length of the string in recursion to slice the anagram and insert the last character in different positions
             for i in range(len(s)):
-                print(s[i])
+                # Slice the anagram in recursion into two parts by [i:] and [:i] and insert the last character of string in between
                 anagrams.append(anagram[i:] + s[-1] + anagram[:i])
+        # Return the anagrams list that will inevitably call anagrams_of(s[:-1]) until a single character remains
         return anagrams
 
 # Test should return the list of all anagrams
