cd34dfd6a2ce261580e6b39ef3aa958a47136e11
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2021-12-21 11:43:56 +0300

    maximum recursion depth may means that maze is solvable

--- hw2/question2.py
+++ hw2/question2.py
@@ -27,23 +27,22 @@ def is_solvable(maze, start, goal):
     current_value = get_value(current_position, maze)
     neighbors = get_neighbors(current_position, maze)
 
-    path = [current_position]
-    for p in path:
-        while get_value(p, maze) == 0:
-            advance(p, maze, path)
-            if p == goal:
-                break
+    paths = []
+    for zero in get_zeros(maze):
+        path = set()
+        advance(zero, maze, path)
+        paths.append(path)
+        if zero == goal:
+            break
 
-    # print(neighbors)
+    print(paths)
 
 
 def get_neighbors(position, maze):
-
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     neighbor_candidates = [(position[1] + dir[0], position[0] + dir[1]) for dir in directions]
     neighbor_candidates = list(filter( lambda p: 0 <= p[0] < len(maze[0]), neighbor_candidates))
     neighbors = list(filter( lambda p: 0 <= p[1] < len(maze), neighbor_candidates))
-
     return neighbors
 
 # print(get_neighbors((7, 7), maze1))
@@ -56,9 +55,19 @@ def get_value(position, maze):
 def advance(position, maze, path):
     if get_value(position, maze) == 1:
         return
-    path.append(position)
+    path.add(position)
     for n in get_neighbors(position, maze):
         return advance(n, maze, path)
+
+def get_zeros(maze):
+    zeros = []
+    for i in range(len(maze)):
+        for j in range(len(maze[0])):
+            if maze[i][j] == 0:
+                zeros.append((j, i))
+    return zeros
+
+# print(get_zeros(maze1))
 
 # Test cases for solvable and not solvable mazes
 print(is_solvable(maze1, (0,0), (7,7)))
