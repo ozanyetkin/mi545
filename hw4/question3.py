c3dbd78ab6e2714984728f6033bfb37c4b4ab583
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-02-01 16:35:52 +0300

    hw4 finished without itertools

--- hw4/question3.py
+++ hw4/question3.py
@@ -4,27 +4,16 @@ example_input = [51, 18, 54, 52, 60]
 
 # Initialize post from pre function to be implemented
 def post_from_pre(preorder):
-    postorder = []
-    while len(preorder) > 0:
-        current = preorder.pop()
-        postorder.append(current)
-        if current > postorder[-1]:
-            postorder.insert(-1, current)
-        else:
-            postorder.insert(0, current)
-    return postorder
-
-import itertools
-
-def post_from_pre(preorder):
     if len(preorder) == 0:
         return []
     root = preorder[0]
     left = []
+    right = []
     for node in preorder[1:]:
-       if node < root:
+        if node < root:
             left.append(node)
-    right = preorder[len(left) + 1:]
+        else:
+            right.append(node)
     return post_from_pre(left) + post_from_pre(right) + [root]
 
 # Test the function which should return [18, 52, 60, 54, 51]
