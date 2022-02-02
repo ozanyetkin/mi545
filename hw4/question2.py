0e11e4f2e04e1ffe1d72e9008b64a8b73b3a8b95
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-02-02 14:32:23 +0300

    all comments finished for hw4, ready for submission

--- hw4/question2.py
+++ hw4/question2.py
@@ -8,48 +8,73 @@ example_input = [4, 3, 2, 6]
 
 # First try, failing only on the last test
 def find_min_cost(stick_list):
+    # Initialize the cost as 0
     cost = 0
+    # Enter a while loop until at most two sticks left in the stick list
     while len(stick_list) > 1:
+        # Sort the sticks using built-in sort method
         stick_list.sort()
+        # Pop the two shortest sticks using first index and store them in s1 and s2 variables
         s1 = stick_list.pop(0)
         s2 = stick_list.pop(0)
+        # Increment the cost by s1 + s2 which corresponds the new combined stick
         cost += s1 + s2
+        # Append the new combined stick to stick list
         stick_list.append(s1 + s2)
+    # Return the calculated cost
     return cost
 
 # Second try, passing all the tests but still a bit slow on the last (2.2 seconds)
 def find_min_cost(stick_list):
+    # Initialize the total cost as 0
     total_cost = 0
+    # Sort the stick list once instead of sorting each time inside the loop
     stick_list.sort()
     
+    # Enter a while loop until at most two sticks left in the stick list
     while len(stick_list) > 1:
+        # Pop the two shortest sticks using first index and store them in s1 and s2 variables
         s1 = stick_list.pop(0)
         s2 = stick_list.pop(0)
+        # Calculate cost by adding two shortest sticks together and add it to total cost
         cost = s1 + s2
         total_cost += cost
-        bisect.insort(stick_list, cost) 
+        # Use built-in bisect module to insert the cost to the correct place in sorted list not to sort each time
+        bisect.insort(stick_list, cost)
+    # Return the calculated total cost
     return total_cost
 
 # Third try, passing all the tests like charm
 def find_min_cost(stick_list):
+    # Initialize the total cost as 0 and an empty temporary list to be used as stack for heap sort
     total_cost = 0
     tmp = []
 
+    # Iterate over each stick in the stick list
     for stick in stick_list:
+        # Use built-in heappush method to construct a heap queue from sticks
         heapq.heappush(tmp, stick)
+
+    # Sort the sticks before entering the loop using heap sort
     sorted_sticks = [heapq.heappop(tmp) for i in range(len(tmp))]
     
+    # Enter a while loop until at most two sticks left in the stick list
     while len(sorted_sticks) > 1:
+        # Use built-in heappop module to pop the two shorted sticks
         s1 = heapq.heappop(sorted_sticks)
         s2 = heapq.heappop(sorted_sticks)
+        # Calculate cost by adding two shortest sticks together and add it to total cost
         cost = s1 + s2
         total_cost += cost
+        # Use built-in heappush module to push the new stick to heap queue
         heapq.heappush(sorted_sticks, cost)
+    # Return the calculated total cost
     return total_cost
 
+# Call the function to test if it calculates the total cost given the example input
 print(find_min_cost(example_input))
 
-# find_min_cost() Tests
+# Run the test harness provided for finding min cost function
 import time
 import random
 
--- hw4/question3.py
+++ hw4/question3.py
@@ -1,25 +1,35 @@
 # Ozan Yetkin | 1908227
-# Initialize example input
+# Initialize the example input provided
 example_input = [51, 18, 54, 52, 60]
 
 # Initialize post from pre function to be implemented
 def post_from_pre(preorder):
+    # Define the base case which is a BST with zero node, return an empty list
     if len(preorder) == 0:
         return []
+    
+    # Initialize the root as the first item since it is guaranteed in preorder
     root = preorder[0]
+    # Initialize left and right as empty lists
     left = []
     right = []
+    # Iterate over each node excluding the root itself
     for node in preorder[1:]:
+        # Check if the node in iteration is smaller than the root
         if node < root:
+            # If it is the case append the node to left
             left.append(node)
+        # Check if the node in iteration is greater than or equal to the root
         else:
+            # If it is the case append the node to right
             right.append(node)
+    # Call the function on left and right to recursively compute all nodes, return left + right + root for postorder
     return post_from_pre(left) + post_from_pre(right) + [root]
 
-# Test the function which should return [18, 52, 60, 54, 51]
+# Test the function with provided example input which should return [18, 52, 60, 54, 51]
 print(post_from_pre(example_input))
 
-example_input = [20, 10, 6, 15, 30, 35]
+example_input = [60, 20, 10, 40, 30, 50, 70]
 
-# Test the function with another input just to be sure which should return [6, 15, 10, 35, 30, 20]
+# Test the function with another input from lecture slides just to be sure, which should return [10, 30, 50, 40, 20, 70, 60]
 print(post_from_pre(example_input))
